5a7927717b5a4032889cb3b098b6e1d5607eed99
xiaokang <cfad@cfaddeMacBook-Pro.local>
2018-09-29 14:04:58 +0800

    添加希尔排序

--- cfyd/jz/base/InsertSort.py
+++ cfyd/jz/base/InsertSort.py
@@ -1,6 +1,7 @@
 #coding:utf-8　
 #直接插入排序
-
+#时间复杂度 O(n的平方)
+#空间复杂度 O(1)
 class Solution:
     def insert_sort(array):
         for i in range(len(array)):
@@ -8,3 +9,8 @@ class Solution:
                 if array[i] < array[j]:
                     array.insert(j,array.pop(i))
         return array
+
+
+
+    print insert_sort([1,0])
+    print eval("1+1-1+2")
---
+++ cfyd/jz/base/ShellSort.py
@@ -0,0 +1,13 @@
+#coding:utf-8　
+class Solution:
+    def shell_sort(array):
+        gap = len(array)
+        while gap > 1:
+            gap = gap // 2
+            for i in range(gap,len(array)):
+                for j in range(i%gap,i,gap):
+                    if array[i] < array[j]:
+                        array[i],array[j] = array[j],array[i]
+        return array
+
+    print shell_sort([0,2,1])
